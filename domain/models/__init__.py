f3683171e95a724362778b87af63a69fdb31fb9c
Andrey Chernykh <andreychernykh04@gmail.com>
2025-04-20 22:36:35 +0300

    rfactor

--- domain/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .chat_completion_model import ChatCompletionModel, Message
-from .completion_model import CompletionModel
-from .data_source import APIClient
-from .embedding_model import EmbeddingModel
-from .model_registry import ModelRegistry
--- main.py
+++ main.py
@@ -2,19 +2,6 @@ import os
 
 import dotenv
 
-import domain.models as models
-
 dotenv.load_dotenv()
 
-api_key = os.getenv("API_KEY")
-client = models.APIClient(api_key)
-registry = models.ModelRegistry(client)
-
-model = registry.get_chat_completion_model("mws-gpt-alpha")
-
-messages = [
-    models.Message(role="system", content="Ты помощник"),
-    models.Message(role="user", content="Привет! Как дела?")
-]
-
-print(model.get_chat_completion(messages, 0.6))
+print(os.getenv("API_KEY"))
